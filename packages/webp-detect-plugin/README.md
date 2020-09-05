eff2f6acbb9a1f52c1568ab738e957b3f80afafd
weilin <2022976289@qq.com>
2020-09-05 18:52:17 +0800

    增加失败时的处理

--- packages/webp-detect-plugin/README.md
+++ packages/webp-detect-plugin/README.md
@@ -9,7 +9,8 @@
 const BrowserDetect = require('webp-detect-webpack-plugin');
 
 // in webpack, you can use this
-// you should use the plugin in html-webpack-plugin after, otherwise it's will not take effect
+// you should use the plugin in html-webpack-plugin after,
+// otherwise it's will not take effect
 plugins: [
   new BrowserDetect(),
 ],
@@ -25,8 +26,10 @@ default option below
   test: /.\.html$/,
   // compress type detect, you can use lossy lossless alpha animation
   feature = 'lossless',
-  // inject code
+  // when support webp, will inject this code
   insertCode = 'document.documentElement.setAttribute("webp", true);window.__detect_webp__ = true;',
+  // when don't support webp, will add this code
+  errorCode = 'document.documentElement.setAttribute("webp", false);window.__detect_webp__ = false;',
   // inject flag, you can define it, but you should add in html page
   // about insertFlag, it's should in head tag inset
   insertFlag = '<head>',
@@ -39,6 +42,7 @@ const isCanUseWebp = document.documentElement.getAttribute('webp') === 'true';
 or
 
 const isCanUseWebp = window.__detect_webp__
+
 ```
 
 ```
@@ -46,8 +50,9 @@ const isCanUseWebp = window.__detect_webp__
 test 就是正则匹配到了的需要注入兼容性代码的文件
 feature webp压缩格式, 有些特殊的压缩格式可能存在兼容, 默认的无损压缩
 insertCode 用来在浏览器中取到的标识判断，建议不要更改, 后续其他的工具可能都会根据此来判断
+errorCode 当浏览器不支持webp格式的标识代码，建议不要更改, 后续其他的工具可能都会根据此来判断
 insertFlag 默认在head标签后追加, 当然你也可以在模板中定义其他占位符, 但一定要在head中靠前的位置
 
-运行时对于支持webp格式的 window.__detect_webp__ 为true
+运行时对于支持webp格式的 window.__detect_webp__ 为true, 否则为 false
 
 ```
--- packages/webp-detect-plugin/index.js
+++ packages/webp-detect-plugin/index.js
@@ -3,25 +3,47 @@
  * webp browser detect
  */
 
+// /**
+//  * 注入能力检测代码段至html页面
+//  * @param {string} insertStr 需要在页面打上的标识
+//  */
+// const insertScriptFn = (insertStr) => `
+// <script>
+//   (function(){
+//     if (document) {
+//       var detectCanvas = document.createElement('canvas') || {};
+//       detectCanvas.width = detectCanvas.height = 1;
+//       var detectFlag = detectCanvas.toDataURL ? detectCanvas.toDataURL('image/webp').indexOf('image/webp') === 5 : false;
+//       if (detectFlag) {
+//         ${insertStr}
+//       }
+//     }
+//   })();
+// </script>`;
 /**
  * 注入能力检测代码段至html页面
- * @param {string} feature 检测哪种压缩形势
+ * @param {string} featureStr 检测哪种压缩形势
  * @param {string} insertStr 需要在页面打上的标识
+ * @param {string} errorStr 加载失败时的处理
  */
-const insertScriptFn = (featureStr, insertStr) => `
-  <script>
-    (function detectWebp () {
-      var img = new Image();
-      img.onload = function () {
-        var result = (img.width > 0) && (img.height > 0);
-        if (result) {
-          ${insertStr}
-        }
-      };
-      img.src = '${featureStr}';
-    })();
-  </script>
-`;
+const insertScriptFn = (featureStr, insertStr, errorStr) => `
+<script>
+  (function () {
+    var img = new Image();
+    img.onload = function () {
+      var result = (img.width > 0) && (img.height > 0);
+      if (result) {
+        ${insertStr}
+      } else {
+        ${errorStr}
+      }
+    };
+    img.onerror = function () {
+      ${errorStr}
+    };
+    img.src = '${featureStr}';
+  })();
+</script>`;
 
 /**
  * webp 几种压缩格式检测支持
@@ -38,10 +60,12 @@ class WebpDetectPlugin {
     test = /.\.html$/,
     feature = 'lossless',
     insertCode = 'document.documentElement.setAttribute("webp", true);window.__detect_webp__ = true;',
+    errorCode = 'document.documentElement.setAttribute("webp", false);window.__detect_webp__ = false;',
     insertFlag = '<head>',
   } = {}) {
     this.feature = feature;
     this.insertCode = insertCode;
+    this.errorCode = errorCode;
     this.test = test;
     this.insertFlag = insertFlag;
   }
@@ -53,8 +77,8 @@ class WebpDetectPlugin {
       assetNames.forEach((fileKey) => {
         if (this.test.test(fileKey)) {
           const fileItem = assetsObj[fileKey];
-          const htmlStr = fileItem.source();
-          if (typeof htmlStr !== 'string') return; 
+          const htmlSource = fileItem.source();
+          let htmlStr = htmlSource.toString();
           let preFix = '';
           if (this.insertFlag === '<head>') preFix = '<head>';
           const targetStr = `
@@ -62,9 +86,16 @@ class WebpDetectPlugin {
             ${insertScriptFn(
               'data:image/webp;base64,' + webpCompressFormatObj[this.feature],
               this.insertCode,
+              this.errorCode,
             )}
           `;
-          const targetSource = htmlStr.replace(this.insertFlag, targetStr);
+
+          // const targetStr = `
+          //   ${preFix}
+          //   ${insertScriptFn(this.insertCode)}
+          // `;
+          let targetSource = htmlStr.replace(this.insertFlag, targetStr);
+          if (typeof htmlSource === 'object') targetSource = Buffer.from(targetSource);
           assetsObj[fileKey] = {
             source: () => targetSource,
             size: () => targetSource.length,
